abe94a09fafc41e8fb0e2b974e06cc7ec8b1f481
Akash ks <akashks8855@gmail.com>
2022-10-29 14:54:19 +0530

    created header component

---
+++ src/components/heading.js
@@ -0,0 +1,15 @@
+
+export default function Heading(){
+    return(
+        <div className="heading">
+
+           <p className="place">kodungallur</p>
+
+           <div className="btn-sec">
+              <button className="searchbtn fa fa-search"></button>
+              <button className="degreebtn">&#8451;</button>
+           </div>
+          
+        </div>
+    )
+}
