02a4b3a29f1465a944f371ff38915c9dc5f84930
Akash ks <akashks8855@gmail.com>
2022-11-02 08:53:10 +0530

    fixed toggle button and hide search btn in clicked hour toggle button

--- src/components/heading.js
+++ src/components/heading.js
@@ -3,11 +3,11 @@ import "../styles/App.css";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import { input } from "../redux/reduxslice";
-
 export default function Heading(){
     
     const dispatch = useDispatch();
     const inputvalue = useSelector((state)=>state.redux.input);
+    const clickdata =useSelector((state)=>state.redux.click);
 
     const [searchclick,setsearchclick] = useState(false);
 
@@ -34,8 +34,8 @@ export default function Heading(){
            <input onKeyDown={getinput} placeholder="Type the cityname" style={{display: searchclick?'block':'none'}} type={"text"} className="search-box"/>
            <button onClick={search} style={{display: searchclick?'block':'none'}} className="close-btn">x</button>
            <div className="btn-sec">
-              <button onClick={search} style={{display: searchclick?'none':'block'}} className="searchbtn fa fa-search"></button>
-              <button className="degreebtn">&#8451;</button>
+              <button onClick={search} style={{display: searchclick?'none':'block',visibility: clickdata?'visible':'hidden'}} className="searchbtn fa fa-search"></button>
+              <button style={{visibility: clickdata?'visible':'hidden'}} className="degreebtn">&#8451;</button>
            </div>
           
         </div>
--- src/components/togglebtn.js
+++ src/components/togglebtn.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import "../styles/toggle.css";
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { click } from '../redux/reduxslice';
 
 
 export default function Togglebtn(){
-
+    
+    const clickdata =useSelector((state)=>state.redux.click);
     const dispatch=useDispatch();
 
     const nowclick=()=>{
       dispatch(click(true));
     }
+
     const hourlyclick=()=>{
         dispatch(click(false));
     }
@@ -19,9 +22,11 @@ export default function Togglebtn(){
         <div className="togglebtn">
 
             <div className="btn-container">
+
                 <button onClick={nowclick} id="now-btn">Now</button>
                 <button onClick={hourlyclick} id="forecast-btn">7 Day Forecast</button>
-                <span className="slider"/>
+                
+                <span style={{transform: clickdata?'translateX(0%)':'translateX(95%)'}} className="slider"/>
             </div>
 
         </div>
